98a7883181eb86078d35adbac47b7443ada101d0
marciojc <468890+marciojc@users.noreply.github.com>
2019-04-18 12:03:10 +0100

    Add search

--- src/Tree.php
+++ src/Tree.php
@@ -10,6 +10,11 @@ class Tree
         $this->root = null;
     }
 
+    /**
+     * Insert a new item into the tree
+     *
+     * @param int $data
+     */
     public function insert($data)
     {
         $node = new Node($data);
@@ -28,6 +33,37 @@ class Tree
         return $this->root->__toString();
     }
 
+    public function traverse($method = 'inorder') : void
+    {
+        switch ($method) {
+            case 'inorder':
+                $this->inorder($this->root);
+                break;
+
+            case 'postorder':
+                $this->postorder($this->root);
+                break;
+
+            case 'preorder':
+                $this->preorder($this->root);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    /**
+     * Search by item data
+     *
+     * @param int $data
+     * @return Node or FALSE
+     */
+    public function search($data)
+    {
+        return $this->searchByKey($data, $this->root);
+    }
+
     private function insertNode(&$root, $node)
     {
         if ($root === null) {
@@ -41,62 +77,68 @@ class Tree
         }
     }
 
-    public function traverse($method = 'inorder') : void {
-        switch($method) {
-             case 'inorder':
-                 $this->inorder($this->root);
-                 break;
-             case 'postorder':
-                 $this->postorder($this->root);
-                 break;
-
-             case 'preorder':
-                 $this->preorder($this->root);
-                 break;
-
-             default:
-             break;
-        }
-    }
-
-    private function inorder($node) : void {
-        if($node->left) {
+    private function inorder($node) : void
+    {
+        if ($node->left) {
             $this->inorder($node->left);
             echo " ";
         }
 
         echo $node;
 
-        if($node->right) {
+        if ($node->right) {
             echo " ";
             $this->inorder($node->right);
         }
     }
 
-    private function preorder($node) : void {
+    private function preorder($node) : void
+    {
         echo $node . " ";
 
-        if($node->left) {
+        if ($node->left) {
             $this->inorder($node->left);
         }
 
-        if($node->right) {
+        if ($node->right) {
             echo " ";
             $this->inorder($node->right);
         }
     }
 
-    private function postorder($node) : void {
-        if($node->left) {
+    private function postorder($node) : void
+    {
+        if ($node->left) {
             $this->inorder($node->left);
             echo " ";
         }
 
-        if($node->right) {
+        if ($node->right) {
             $this->inorder($node->right);
             echo " ";
         }
 
         echo $node;
+    }
+
+    /**
+     *
+     * @param int $data
+     * @param Node $node
+     * @return FALSE or Node
+     */
+    protected function searchByKey($data, &$node)
+    {
+        if ($node == null) {
+            return false;
+        }
+
+        if ($node->data === $data) {
+            return $node;
+        } else if ($node->data > $data) {
+            return $this->searchByKey($data, $node->left);
+        } else {
+            return $this->searchByKey($data, $node->right);
+        }
     }
 }
