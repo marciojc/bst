305c62f734e48120eb61d05f883e9ccc81baf9b4
marciojc <468890+marciojc@users.noreply.github.com>
2019-04-20 16:20:43 +0100

    Add test to BitArray insert

--- tests/BitArrayTest.php
+++ tests/BitArrayTest.php
@@ -7,7 +7,39 @@ final class BitArrayTest extends TestCase
 {
     public function testBitArrayEmpty(): void
     {
-        $tree = new Bst\BitArray();
-        $this->assertEmpty($tree->data);
+        $ba = new Bst\BitArray();
+        $this->assertEmpty($ba->data);
+    }
+
+    public function testBitArrayRoot(): void
+    {
+        $ba = new Bst\BitArray();
+        $ba->insert(5);
+        $this->assertEquals($ba->data, array(5));
+    }
+
+    public function testBitArrayRootLeft(): void
+    {
+        $ba = new Bst\BitArray();
+        $ba->insert(5);
+        $ba->insert(3);
+        $this->assertEquals($ba->data, array(5, 3));
+    }
+
+    public function testBitArrayRootRight(): void
+    {
+        $ba = new Bst\BitArray();
+        $ba->insert(5);
+        $ba->insert(7);
+        $this->assertEquals($ba->data, array( 0 => 5, 2 => 7));
+    }
+
+    public function testBitArray(): void
+    {
+        $ba = new Bst\BitArray();
+        $ba->insert(5);
+        $ba->insert(3);
+        $ba->insert(7);
+        $this->assertEquals($ba->data, array(5, 3, 7));
     }
 }
